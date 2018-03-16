de5c2d1d43234f932686748349c982ad0cfea1a1
VLADISLAV <vladislavi@mail.ru>
2018-03-16 04:17:21 +0000

    Update luchano_gl8.py

--- luchano_gl8.py
+++ luchano_gl8.py
@@ -22,3 +22,18 @@ e = e+'r'
 print(id(e),e)
 print(id(f),f)
 
+t1 = (1,2,[30,40])
+t1[-1].append(99)
+print(t1)
+l1 = [1,2,3,[1,2]]
+l2 = list(l1)
+print(l2,l1)
+l1 = [3,[77,55,44],(7,8,9)]
+l2 = list(l1)
+l1.append(100)
+l1[1].remove(55)
+print(l1)
+print(l2)
+l2[1] += [33,22]
+print(l1)
+print(l2)
