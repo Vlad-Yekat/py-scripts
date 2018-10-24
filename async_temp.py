abd648679031083760ad8dac2b4867c5ca86b051
Vlad <vladislavi@mail.ru>
2018-10-24 22:24:42 +0500

    some 2410

--- async_temp.py
+++ async_temp.py
@@ -1,19 +1,21 @@
 # requirement python 3.7.1
+# template for change data in parallel executions
 import asyncio
 
-def inner_proc(lst):
+
+async def inner_proc(lst):
     print(lst)
 
 
 async def main():
 
-    accounts = ['123', '124', '125', '126', '127', '128', '129', '130', '131', '132']  # etc
-    n = 8 #кол-во shared_future
+    accounts = ['123', '124', '125', '126', '127', '128', '129', '130', '131', '132', '133', '134']  # etc
+    n = 8  # кол-во shared_future
     futures = []
     if len(accounts) > 0:
         lst_lst = [accounts[i::n] for i in range(n)]
         for lst in lst_lst:
-            #параллельный вызов метода со скриптом на каждой БД
+            # параллельный вызов метода со скриптом на каждой БД
             future = asyncio.create_task(inner_proc(lst))
             futures.append(future)
         # дожидаемся выполнения всех вызовов
